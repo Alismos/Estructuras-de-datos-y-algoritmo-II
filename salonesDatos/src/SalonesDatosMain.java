1de52709d5a5028165c3e015b1bd7b851dd825a9
Shiroke-013 <Shiroke4321@gmail.com>
2019-11-20 16:38:00 -0500

    Matriz de distancias bloques

--- salonesDatos/src/SalonesDatosMain.java
+++ salonesDatos/src/SalonesDatosMain.java
@@ -4,27 +4,35 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class SalonesDatosMain{
-    public static void main(String [] args){
-        leer_archivo("D:/Alismos/Documentos/GitHub/Estructuras-de-datos-y-algoritmo-II/aulas.csv");
+    public static void main(String [] args) {
+        leer_archivo("DistanciasBloques.csv");
     }
 
-    public HashMap<HashMap<>, >
     public static void leer_archivo(String archivo){
         BufferedReader bufferLectura = null;
         try {
+            int[][] distances = new int[39][39];
             // Abrir el .csv en buffer de lectura
             bufferLectura = new BufferedReader(new FileReader(archivo));
 
             // Leer una linea del archivo
             String linea = bufferLectura.readLine();
             while (linea != null) {
-                // Sepapar la linea leída con el separador definido previamente
+                // Separar la linea leída con el separador definido previamente
                 String[] campos = linea.split(",");
-
-                System.out.println(Arrays.toString(campos));
-
+                //Poner en la matriz "distances" la distancia entre dos bloques, los cuales son representados
+                // con filas y columnas
+                distances[Integer.parseInt(campos[0])][Integer.parseInt(campos[1])] = Integer.parseInt(campos[2]);
+                distances[Integer.parseInt(campos[1])][Integer.parseInt(campos[0])] = Integer.parseInt(campos[2]);
+                System.out.println(campos[0]+" "+campos[1] +" "+ campos[2]);
                 // Volver a leer otra línea del fichero
                 linea = bufferLectura.readLine();
+            }
+            for(int i = 0; i < 39; i++){
+                for(int j = 0; j < 39; j++){
+                    System.out.print(distances[i][j]+ " ");
+                }
+                System.out.println();
             }
         }
         catch (IOException e) {
