884201fdca03930ae7f78326b87382f63a757e69
Shiroke-013 <Shiroke4321@gmail.com>
2019-11-25 05:20:59 -0500

    "Last changes, thinking that the problem is done"

--- salonesDatos/src/Student.java
+++ salonesDatos/src/Student.java
@@ -1,14 +1,16 @@
 public class Student {
     private int id; // Means the number that represents each student
-    private String subject; //
-    private String group;
+    private String subject; // The subject of the class
+    private String group; // The group the student is in seeing the class
 
+    // Constructors of the object Student
     public Student(int id, String subject, String group) {
         this.id = id;
         this.subject = subject;
         this.group = group;
     }
 
+    // Methods that return the information you are asking for
     public int getId() {
         return id;
     }
@@ -21,6 +23,7 @@ public class Student {
         return group;
     }
 
+    // Methods that change the values of the attributes of the object
     public void setId(int id) {
         this.id = id;
     }
